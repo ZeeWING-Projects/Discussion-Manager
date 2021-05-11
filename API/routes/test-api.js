8bb74752e0e337a805c5e0dcc9e380dae7a76aba
Zeeshan <66442918+ZeeTheGamer@users.noreply.github.com>
2021-05-12 00:29:39 +0500

    updated resources

--- API/routes/test-api.js
+++ API/routes/test-api.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/",function(req,res,next){
-    res.send("API is working properly")
+    res.send("API is working properly");
 });
+
 
 module.exports=router;
 
