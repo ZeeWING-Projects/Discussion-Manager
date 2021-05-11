ebc2326c244c40eb491d8c9d86388696e7431dea
Zeeshan <66442918+ZeeTheGamer@users.noreply.github.com>
2021-05-11 20:07:04 +0500

    Created Base of Client and API Application
    Note that when you are fetching it then you need to install the all packages to get node_modules folder

---
+++ Client-App/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { render } from '@testing-library/react';
+
+
+class App extends React.Component{
+  constructor(props)
+  {
+    super(props)
+    this.state={apiResponse:""} //This will create this varaible
+
+  }
+
+  callAPI()
+  {
+    fetch('http://localhost:9000/test-api').then((resp)=>{
+      return resp.text()//this will get the response.
+    }).then((response)=>{
+      this.setState({apiResponse:response})//this is the reponse which is recieved from inner API
+    })
+  }
+
+  componentWillMount()
+  {
+    this.callAPI()
+  }
+
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <p>{this.state.apiResponse}</p>
+    </div>
+  );
+}
+}
+
+export default App;
