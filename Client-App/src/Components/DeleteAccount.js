651d2dd99723c33552fb41b1e13df0467bcf2576
Tuba Asif <68794712+TAProgramer@users.noreply.github.com>
2021-06-04 03:45:00 +0500

    deleteACC_module_design_nd_working_1

--- Client-App/src/Components/DeleteAccount.js
+++ Client-App/src/Components/DeleteAccount.js
@@ -3,15 +3,76 @@ import { Router,Link } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container,Row,Col,Card,Button,Form,FormControl,Image} from 'react-bootstrap'
 import styles from './mystyle.module.css'; 
+import { Label } from "reactstrap";
 
 export default function DeleteAccount(){
     return (
         <div  style={{
+            marginTop:"15%",
+            marginLeft:"10%",
+            color:"white",
+            borderStyle: 'solid',
+            borderColor:'rgb(201, 164, 164)',
+            borderWidth: "5px",
+            height:"60%",
+            width:"100%"  
+         }}>
+        <Row>
+            <Col md={8}>
+           
+        <Form style={{
             marginTop:"10%",
-            backgroundColor:"white",
-            fontSize:"100px"
+           marginLeft:"20%",
+           color:"white"
+           
         }}>
-        <h1>this is Delete Account</h1>
+        
+  <Form.Group controlId="previousPass">
+    <Form.Label  style={{
+            marginRight:"30%",
+           color:"white"
+           
+        }}>Enter Your Password to confirm: </Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  </Form>
+  </Col>
+
+  <Col md={3}>
+
+  <Button variant="dark"style={{
+            marginTop:"43%",
+            marginRight:"80%",
+        }}>
+    Check
+  </Button>
+  </Col>
+</Row>
+  <Row>
+      <Col md={8}>
+      <Label style={{
+           marginTop:"1%",
+           color: "red",
+           fontSize:"20px",
+           marginLeft:"5%",
+        }}>
+          Are you sure you want to delete this account?
+      </Label>
+      </Col>
+      </Row>
+  <Row>
+      <Col md={4}>
+  <Button variant="dark" size="lg" block style={{
+            marginTop:"5%",
+            marginLeft:"80%",
+        }}>
+    Delete Account
+  </Button>
+  </Col>
+</Row>
+
+
         </div>
+      
     );
 }
