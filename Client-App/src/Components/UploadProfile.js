bad7485ff8fd87834fc2c5e37a74e85dc27f7763
Tuba Asif <68794712+TAProgramer@users.noreply.github.com>
2021-06-04 01:45:35 +0500

    uploadImage_design_and_working_1

--- Client-App/src/Components/UploadProfile.js
+++ Client-App/src/Components/UploadProfile.js
@@ -1,21 +1,60 @@
 import React,{useEffect,useState} from "react";
 import { Router,Link } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import profile_image from './default_image.png'
+import profile_image from './ImagePreview_logo.png'
 import {Container,Row,Col,Card,Button,Form,FormControl,Image} from 'react-bootstrap'
 import styles from './mystyle.module.css'; 
+import { Label } from "reactstrap";
 
 export default function UploadProfile(){
+    let imagePreview="Image Preview"
     return (
         <div  style={{
             marginTop:"10%",
            
         }}>
+            <Row>
+                <Col md={8}>
          <input type="file"  style={{
             color:"white",
+            marginLeft: "60%",
+            
         }}/> 
-        <Image src={profile_image} roundedCircle />
-    <div></div>
+        </Col>
+        </Row>
+        <Row>
+        <Col md={12}>
+            <div>
+                
+        <Image src={profile_image} roundedCircle style={{
+          marginTop:"5%"
+            
+        }} />
+        </div>
+        </Col>
+   </Row>
+        <Row>
+            <Col md={12}>
+                <Label style={{
+    marginTop: "3%",
+   fontSize: "25px",
+   color:"White"
+}}>
+    {imagePreview}
+</Label>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={4}>
+            <Button variant="dark" size="lg" block style={{
+                              marginTop: "5%",  
+                              marginLeft: "115%"
+                            }} >
+
+ Set Image </Button>
+ </Col>
+                </Row>
         </div>
     );
 }
+
